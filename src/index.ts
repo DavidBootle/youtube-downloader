1b170689fe9421c87df07bccd8e417753d52bf25
David Bootle <davidtbootle@gmail.com>
2021-11-05 19:11:17 -0400

    added static files

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 8002;
 
+app.use("/static", express.static('public'));
+
 app.get( "/", (req, res) => {
     res.send('Hello world!');
 });
