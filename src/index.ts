040881f2d3611f634d0e05f458c812ffa0c69843
David Bootle <davidtbootle@gmail.com>
2021-11-05 20:18:28 -0400

    added header and footer to index page

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = process.env.PORT || 8002;
 
 app.use("/static", express.static('public'));
 
 app.get( "/", (req, res) => {
-    res.send('Hello world!');
+    res.sendFile(path.join(__dirname, '../html/index.html'));
 });
 
 app.listen( port, () => {
